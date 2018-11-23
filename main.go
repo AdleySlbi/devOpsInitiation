34dfb42e138df93d06222ed425deef5692c9f4cc
Adley <adley.salabi@gmail.com>
2018-11-23 15:48:49 +0100

    Link between git and herokuku

--- main.go
+++ main.go
@@ -15,7 +15,12 @@ func main() {
 		port = "8080"
 	}
 
+	// connection := os.Getenv("DATABASE_URL")
+	fmt.Println("Tout va bien")
+	fmt.Println("Everything is going well well")
+
 	fmt.Println("Server is running at ", port)
+
 	myHandler := handler.NewHandler()
 
 	server := &http.Server{
